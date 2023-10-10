6a446f9a482f3997ec17ef8bd02ac8023c70c8cc
Prasetyo <prasetyosyifa@gmail.com>
2023-10-10 16:25:24 +0700

    change data

--- src/pages/SettingGroupData/SettingGroupData.tsx
+++ src/pages/SettingGroupData/SettingGroupData.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Header, TableV2 } from "../../components";
-import { columnsDummyAll, dataSourceDummy } from "../../data/basic";
+import { columnsDummyNoAction, dataSourceDummy } from "../../data/basic";
 
 const SettingGroupData: React.FC = () => {
   return (
     <div style={{ margin: "0 15px" }}>
       <Header title="Menu Group Data Uncontrolled" />
       <TableV2
-        defaultColumns={columnsDummyAll}
+        defaultColumns={columnsDummyNoAction}
         defaultDataSource={dataSourceDummy}
         resizable={false}
         selectionMode="multiselect"
